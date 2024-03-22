70e5eded084c9eda4a7d2a6fa011b761302dc129
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-03-23 01:50:25 +0300

    implement LatinQuotesStorage and RomanNumbersValidator methods

--- src/main/java/ru/romanov/romancalc/roman/RomanNumbersValidatorImpl.java
+++ src/main/java/ru/romanov/romancalc/roman/RomanNumbersValidatorImpl.java
@@ -3,8 +3,14 @@ package ru.romanov.romancalc.roman;
 import ru.romanov.romancalc.alerts.AlertsManager;
 import ru.romanov.romancalc.calculator.MixedFraction;
 
+import java.util.regex.Pattern;
+
 public class RomanNumbersValidatorImpl implements RomanNumbersValidator{
 
+    private static final Pattern PATTERN_OF_ROMAN_NUMBER_WITH_FRACTION_PART = Pattern.compile(
+            "^(M̅){0,3}((C̅M̅)|(C̅D̅)|(D̅)?(C̅){0,3})((X̅C̅)|(X̅L̅)|(L̅)?(X̅){0,3})((MX̅)|(MV̅)|(V̅)?M{0,3})" +
+                    "(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})S?•{0,5}Є?Ɔ?[Ƨƻ]?℈?»{0,5}$"
+    );
     private final AlertsManager alertsManager;
 
     public RomanNumbersValidatorImpl(AlertsManager alertsManager) {
@@ -13,11 +19,19 @@ public class RomanNumbersValidatorImpl implements RomanNumbersValidator{
 
     @Override
     public boolean isNumberValid(String romanNumber) {
-        return false;
+        return PATTERN_OF_ROMAN_NUMBER_WITH_FRACTION_PART.matcher(romanNumber).matches();
     }
 
     @Override
-    public boolean isArabicNumberPossibleConvertToRoman(MixedFraction arabicNumber) {
-        return false;
+    public boolean isArabicNumberPossibleConvertToRoman(MixedFraction fraction) {
+        if (fraction.getFullPart() < 0) {
+            alertsManager.showNegativeAlert();
+            return false;
+        }
+        if (fraction.getFullPart() > 3999999) {
+            alertsManager.showBigSizeAlert(fraction.getFullPart());
+            return false;
+        }
+        return true;
     }
 }
