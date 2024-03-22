f4dd06aea834aaafae4ee1985e9b22a3a207ed55
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-03-23 01:04:23 +0300

    implement Calculator methods, replace Calculator test-cases from csv to annotations

--- src/main/java/ru/romanov/romancalc/calculator/FractionCalculatorImpl.java
+++ src/main/java/ru/romanov/romancalc/calculator/FractionCalculatorImpl.java
@@ -1,23 +1,52 @@
 package ru.romanov.romancalc.calculator;
 
+import java.math.BigInteger;
+
 public class FractionCalculatorImpl implements FractionCalculator{
     @Override
     public MixedFraction addict(MixedFraction input1, MixedFraction input2) {
-        return null;
+        long numeratorOfFirstInput = convertFractionToImproperAndGetNumerator(input1);
+        long numeratorOfSecondInput = convertFractionToImproperAndGetNumerator(input2);
+        long numeratorOfImproperFraction = numeratorOfFirstInput + numeratorOfSecondInput;
+        return convertImproperFractionToMixedFraction(numeratorOfImproperFraction);
     }
 
     @Override
     public MixedFraction subtract(MixedFraction input1, MixedFraction input2) {
-        return null;
-    }
-
-    @Override
-    public MixedFraction multiply(MixedFraction input1, MixedFraction input2) {
-        return null;
+        long numeratorOfFirstInput = convertFractionToImproperAndGetNumerator(input1);
+        long numeratorOfSecondInput = convertFractionToImproperAndGetNumerator(input2);
+        long numeratorOfImproperFraction = numeratorOfFirstInput - numeratorOfSecondInput;
+        return convertImproperFractionToMixedFraction(numeratorOfImproperFraction);
     }
 
     @Override
     public MixedFraction divide(MixedFraction input1, MixedFraction input2) {
-        return null;
+        long numeratorOfFirstInput = convertFractionToImproperAndGetNumerator(input1);
+        long numeratorOfSecondInput = convertFractionToImproperAndGetNumerator(input2);
+        long numeratorOfImproperFraction =
+                (numeratorOfFirstInput * input1.getDenominator()) / numeratorOfSecondInput;
+        return convertImproperFractionToMixedFraction(numeratorOfImproperFraction);
+    }
+
+    @Override
+    public MixedFraction multiply(MixedFraction input1, MixedFraction input2) {
+        long numeratorOfFirstInput = convertFractionToImproperAndGetNumerator(input1);
+        long numeratorOfSecondInput = convertFractionToImproperAndGetNumerator(input2);
+        long numeratorOfImproperFraction = BigInteger
+                .valueOf(numeratorOfFirstInput)
+                .multiply(BigInteger.valueOf(numeratorOfSecondInput))
+                .divide(BigInteger.valueOf(1728))
+                .longValue();
+        return convertImproperFractionToMixedFraction(numeratorOfImproperFraction);
+    }
+
+    private long convertFractionToImproperAndGetNumerator(MixedFraction fraction) {
+        return (fraction.getFullPart() * fraction.getDenominator()) + fraction.getNumerator();
+    }
+
+    private MixedFraction convertImproperFractionToMixedFraction(long numeratorOfImproperFraction) {
+        long fullPartOfMixedFraction = numeratorOfImproperFraction / 1728;
+        int numeratorOfMixedFraction = (int) (numeratorOfImproperFraction % 1728);
+        return new MixedFraction(fullPartOfMixedFraction, numeratorOfMixedFraction);
     }
 }
--- src/test/java/ru/romanov/romancalc/calculator/CalculatorTest.java
+++ src/test/java/ru/romanov/romancalc/calculator/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,7 +21,13 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/calculator-addition-test-data.csv", delimiter = ';', numLinesToSkip = 1)
+    @CsvSource(value = {
+            "1, 0, 0, 1, 1, 1",
+            "3, 2, 4, 88, 7, 90",
+            "5, 1727, 0, 1, 6, 0",
+            "3999999, 1727, 3999999, 1727, 7999999, 1726",
+            "5, 864, 5, 865, 11, 1"
+    })
     void additionTest(long number1FullPart, int number1FractionPart,
                       long number2FullPart, int number2FractionPart,
                       long expectedResultFullPart, int expectedResultFractionPart) {
@@ -35,12 +41,18 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/calculator-subtraction-test-data.csv", delimiter = ';', numLinesToSkip = 1)
+    @CsvSource(value = {
+            "7, 7, 7, 7, 0, 0",
+            "5, 0, 0, 1, 4, 1727",
+            "1, 1, 1, 0, 0, 1",
+            "3999999, 864, 99, 1727, 3999899, 865",
+            "1, 0, 2, 0, -1, 0"
+    })
     void subtractionTest(long number1FullPart, int number1FractionPart,
                          long number2FullPart, int number2FractionPart,
                          long expectedResultFullPart, int expectedResultFractionPart) {
 
-        MixedFraction result = calculator.addict(
+        MixedFraction result = calculator.subtract(
                 new MixedFraction(number1FullPart, number1FractionPart),
                 new MixedFraction(number2FullPart, number2FractionPart));
 
@@ -49,12 +61,18 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/calculator-multiplication-test-data.csv", delimiter = ';', numLinesToSkip = 1)
+    @CsvSource(value = {
+            "1, 0, 1, 0, 1, 0",
+            "3999999, 0, 0, 864, 1999999, 864",
+            "25, 6, 100, 144, 2502 , 744",
+            "3000000, 0, 3000000, 0, 9000000000000, 0",
+            "1234, 56, 78, 90, 96318, 1382"
+    })
     void multiplicationTest(long number1FullPart, int number1FractionPart,
                             long number2FullPart, int number2FractionPart,
                             long expectedResultFullPart, int expectedResultFractionPart) {
 
-        MixedFraction result = calculator.addict(
+        MixedFraction result = calculator.multiply(
                 new MixedFraction(number1FullPart, number1FractionPart),
                 new MixedFraction(number2FullPart, number2FractionPart));
 
@@ -63,12 +81,18 @@ public class CalculatorTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/calculator-division-test-data.csv", delimiter = ';', numLinesToSkip = 1)
+    @CsvSource(value = {
+            "5, 864, 1, 0, 5, 864",
+            "5, 432, 0, 864, 10, 864",
+            "999, 0, 3, 0, 333, 0",
+            "22, 864, 11, 432, 2, 0",
+            "3999999, 1727, 421, 1, 9501, 301"
+    })
     void divisionTest(long number1FullPart, int number1FractionPart,
                       long number2FullPart, int number2FractionPart,
                       long expectedResultFullPart, int expectedResultFractionPart) {
 
-        MixedFraction result = calculator.addict(
+        MixedFraction result = calculator.divide(
                 new MixedFraction(number1FullPart, number1FractionPart),
                 new MixedFraction(number2FullPart, number2FractionPart));
 
