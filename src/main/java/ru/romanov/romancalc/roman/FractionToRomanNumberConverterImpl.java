35a55f45d88b2b902bc83bc4f3e254a031ea6c82
Dmitry Romanov <this.romanovdmitry@gmail.com>
2024-03-23 01:35:43 +0300

    implement RomanToFractionConverter and FractionToRomanConverter methods

--- src/main/java/ru/romanov/romancalc/roman/FractionToRomanNumberConverterImpl.java
+++ src/main/java/ru/romanov/romancalc/roman/FractionToRomanNumberConverterImpl.java
@@ -2,9 +2,75 @@ package ru.romanov.romancalc.roman;
 
 import ru.romanov.romancalc.calculator.MixedFraction;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FractionToRomanNumberConverterImpl implements FractionToRoman{
+
+    private final Map<Character, Integer> romanFractionsAndValues = getRomanFractionsAndValuesMap();
+
     @Override
     public String convertFractionToRomanNumber(MixedFraction arabicNumber) {
-        return null;
+        if (arabicNumber.isZero()) return "nulla";
+
+        String romanFullPartOfNumber = fullPartToRoman((int) arabicNumber.getFullPart());
+        String romanFractionPartOfNumber = fractionPartToRoman(arabicNumber.getNumerator());
+
+        return romanFullPartOfNumber + romanFractionPartOfNumber;
     }
+
+    private String fullPartToRoman(int full) {
+        if (full == 0) return "";
+        String[] M100 = {"", "M̅", "M̅M̅", "M̅M̅M̅"};
+        String[] C100 = {"", "C̅", "C̅C̅", "C̅C̅C̅", "C̅D̅", "D̅", "D̅C̅", "D̅C̅C̅", "D̅C̅C̅C̅", "C̅M̅"};
+        String[] X100 = {"", "X̅", "X̅X̅", "X̅X̅X̅", "X̅L̅", "L̅", "L̅X̅", "L̅X̅X̅", "L̅X̅X̅X̅", "X̅C̅"};
+        String[] M =    {"", "M", "MM", "MMM", "MV̅", "V̅", "V̅M", "V̅MM", "V̅MMM", "MX̅"};
+        String[] C =    {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] X =    {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] I =    {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(M100[full/1000000])
+                .append(C100[full%1000000/100000])
+                .append(X100[full%100000/10000])
+                .append(M[full%10000/1000])
+                .append(C[(full%1000)/100])
+                .append(X[(full%100)/10])
+                .append(I[full%10]);
+
+        return sb.toString();
+    }
+
+    private String fractionPartToRoman(int numerator) {
+        if (numerator == 0) return "";
+
+        char[] fractionDigits = {'S', '•', 'Є', 'Ɔ', 'Ƨ', 'ƻ', '℈', '»'};
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0 ; i < fractionDigits.length ; i++) {
+            char currentChar = fractionDigits[i];
+            int value = romanFractionsAndValues.get(currentChar);
+            while (numerator >= value) {
+                sb.append(currentChar);
+                numerator -= value;
+            }
+        }
+        return sb.toString();
+    }
+
+    private Map<Character, Integer> getRomanFractionsAndValuesMap() {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('S', 864);
+        map.put('•', 144);
+        map.put('Є', 72);
+        map.put('Ɔ', 36);
+        map.put('Ƨ', 24);
+        map.put('ƻ', 12);
+        map.put('℈', 6);
+        map.put('»', 1);
+
+        return map;
+    }
+
+
 }
--- src/main/java/ru/romanov/romancalc/roman/RomanNumberToFractionConverterImpl.java
+++ src/main/java/ru/romanov/romancalc/roman/RomanNumberToFractionConverterImpl.java
@@ -2,9 +2,97 @@ package ru.romanov.romancalc.roman;
 
 import ru.romanov.romancalc.calculator.MixedFraction;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumberToFractionConverterImpl implements RomanToFraction{
+
+    private final Map<Character, Integer> romanDigitsAndValues = getRomanDigitsAndValues();
+
+    private final Map<Character, Integer> romanFractionDigitsAndValues = getRomanFractionDigitsAndValues();
+
     @Override
     public MixedFraction convertRomanNumberToFraction(String romanNumber) {
-        return null;
+        if (romanNumber.equals("nulla"))
+            return MixedFraction.zeroFraction();
+
+        String romanFractionPart = getFractionPart(romanNumber);
+        String romanFullPart = romanNumber.replace(romanFractionPart, "");
+
+        long fullPartOfFraction = fullRomanNumberToArabicNumber(romanFullPart);
+        int numeratorOfFraction = fractionRomanNumberToArabicFractionNumerator(romanFractionPart);
+
+        return new MixedFraction(fullPartOfFraction, numeratorOfFraction);
+    }
+
+    private long fullRomanNumberToArabicNumber(String input) {
+        if (input.length() == 0) return 0;
+        String convertable = replaceDigitsWithSpecialSymbolsToSimpleDigits(input);
+
+        long result = 0;
+        for (int i = 0; i < convertable.length() - 1; i++) {
+            char currentDigit = convertable.charAt(i);
+            char nextDigit = convertable.charAt(i+1);
+            if(romanDigitsAndValues.get(currentDigit) < romanDigitsAndValues.get(nextDigit))
+                result -= romanDigitsAndValues.get(currentDigit);
+            else
+                result += romanDigitsAndValues.get(currentDigit);
+        }
+        return result + romanDigitsAndValues.get(convertable.charAt(convertable.length() - 1));
+    }
+
+    private int fractionRomanNumberToArabicFractionNumerator(String input) {
+        if (input.length() == 0) return 0;
+
+        int divider = 0;
+        for (int i = 0 ; i < input.length() ; i++) {
+            divider += romanFractionDigitsAndValues.get(input.charAt(i));
+        }
+        return divider;
+    }
+
+    private String getFractionPart(String input) {
+        return input
+                .replace("I", "").replace("V", "").replace("X", "")
+                .replace("L", "").replace("C", "").replace("D", "")
+                .replace("M", "").replace("̅", "");
+    }
+
+    public String replaceDigitsWithSpecialSymbolsToSimpleDigits(String input) {
+        return input
+                .replace("V̅", "v").replace("X̅", "x")
+                .replace("L̅", "l").replace("C̅", "c")
+                .replace("D̅", "d").replace("M̅", "m");
+    }
+
+    private Map<Character, Integer> getRomanDigitsAndValues() {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        map.put('v', 5000);
+        map.put('x', 10000);
+        map.put('l', 50000);
+        map.put('c', 100000);
+        map.put('d', 500000);
+        map.put('m', 1000000);
+        return map;
+    }
+
+    private Map<Character, Integer> getRomanFractionDigitsAndValues() {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('S', 864);
+        map.put('•', 144);
+        map.put('Є', 72);
+        map.put('Ɔ', 36);
+        map.put('Ƨ', 24);
+        map.put('ƻ', 12);
+        map.put('℈', 6);
+        map.put('»', 1);
+        return map;
     }
 }
